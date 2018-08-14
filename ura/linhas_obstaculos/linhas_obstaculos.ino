d5da429cf210a89b954ee068d43b6ab8441071a1
bcq <bcq.cpp@gmail.com>
2018-08-13 22:44:29 -0300

    seguir linhas e desviar desviar de obstaculos | teste

---
+++ ura/linhas_obstaculos/linhas_obstaculos.ino
@@ -0,0 +1,71 @@
+#include <Servo.h>
+#include <NewPing.h>
+
+#define snsE 0
+#define snsD 3
+#define pTrig 9
+#define pEcho 10
+
+int dist;
+
+Servo esquerdo;
+Servo direito;
+NewPing sonar(pTrig, pEcho);
+
+void frente (){
+  esquerdo.write(1);
+  direito.write(180);
+}
+
+void esquerda(){
+  esquerdo.write(180);
+  direito.write(180);
+}
+
+void direita(){
+  esquerdo.write(1);
+  direito.write(1);
+}
+
+void parar(int tempo){
+  esquerdo.detach();
+  direito.detach();
+  delay(tempo);
+}
+
+void setup(){
+  esquerdo.attach (7);
+  direito.attach (5);
+  pinMode (snsE, INPUT);
+  pinMode (snsD, INPUT);
+}
+
+void loop(){
+  int s1=digitalRead(snsE);        //sensor esquerdo
+  int s2=digitalRead(snsD);        //sensor direito
+  dist=sonar.ping_cm();
+
+  if (dist<10 && dist>2){
+    parar(500);
+    direita();
+      delay(800);
+    frente();
+      delay(1000);
+    esquerda();
+      delay(800);
+    frente();
+      delay(1200);
+    esquerda();
+      delay(800);
+    frente();
+      delay(1000);
+    direita();
+      delay(800);
+  } else if (s1==0 && s2==0){                //se os dois sensores virem branco
+    frente();
+  } else if (s1==1 && s2==0){        //se o sensor esquerdo vir preto
+    esquerda();
+  } else if (s1==0 && s2==1){       //se o sensor direito vir preto
+    direita();
+  }
+}
