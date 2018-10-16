9036e5b5a70b6bc9d606f0bf0d51f59b70e397aa
bcq <bcq.cpp@gmail.com>
2018-10-16 05:36:23 -0300

    uahhhh

--- pisca_pisca/pisca_pisca.ino
+++ pisca_pisca/pisca_pisca.ino
@@ -2,9 +2,9 @@
 
 #define redOne 3
 #define yellowOne 4
-#define blueOne = 5
-#define greenOne = 6
-#define whiteOne = 7
+#define blueOne 5
+#define greenOne 6
+#define whiteOne 7
 #define redTwo 8
 #define yellowTwo 9
 #define blueTwo10
@@ -15,7 +15,18 @@
 
 
 void setup(){
-
+  pinMode(redOne, OUTPUT);
+  pinMode(yellowOne, OUTPUT);
+  pinMode(blueOne, OUTPUT);
+  pinMode(greenOne, OUTPUT);
+  pinMode(whiteOne, OUTPUT);
+  pinMode(redTwo, OUTPUT);
+  pinMode(yellowTwo, OUTPUT);
+  pinMode(blueTwo, OUTPUT);
+  pinMode(greenTwo, OUTPUT);
+  pinMode(whitetTwo, OUTPUT;
 }
 
-void loop(){}
+void loop(){
+  
+}
