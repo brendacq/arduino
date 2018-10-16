b4da282a5ff382a0c4a047cf4a5c87d13d95fc1d
bcq <bcq.cpp@gmail.com>
2018-10-16 05:30:49 -0300

    initial commit

---
+++ pisca_pisca/pisca_pisca.ino
@@ -0,0 +1,21 @@
+//Programando um pisca-pisca
+
+#define redOne 3
+#define yellowOne 4
+#define blueOne = 5
+#define greenOne = 6
+#define whiteOne = 7
+#define redTwo 8
+#define yellowTwo 9
+#define blueTwo10
+#define greenTwo 11
+#define whiteTwo 12
+//#define orangeOne
+//#define orangeTwo
+
+
+void setup(){
+
+}
+
+void loop(){}
